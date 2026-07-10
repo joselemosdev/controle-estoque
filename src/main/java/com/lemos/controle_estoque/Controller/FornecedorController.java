758e325883ddfd3b59f06429150ed9df553b0ba2
José Eduardo <jose.lemos.dev>
2026-07-10 11:29:04 -0300

    Add endpoints e busca de fornecedor por id e geral

--- src/main/java/com/lemos/controle_estoque/Controller/FornecedorController.java
+++ src/main/java/com/lemos/controle_estoque/Controller/FornecedorController.java
@@ -1,16 +1,29 @@
 package com.lemos.controle_estoque.Controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lemos.controle_estoque.Domain.Fornecedor;
+import com.lemos.controle_estoque.Service.FornecedorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fornecedor")
 public class FornecedorController {
 
+    private final FornecedorService service;
+    public FornecedorController(FornecedorService fornecedorService){
+        this.service = fornecedorService;
+    }
+
+    @GetMapping
+    private List<Fornecedor> getFornecedores(){
+        return service.getFornecedores();
+    }
+
     @GetMapping("/{id}")
-    private String getFornecedor(){
-        return "Endpoint de fornecedor funcionando";
+    private Optional<Fornecedor> getFornecedor(@PathVariable("id") String id){
+        return service.getFornecedorById(id);
     }
 }
---
+++ src/main/java/com/lemos/controle_estoque/Repository/MovimentoEstoqueRepository.java
@@ -0,0 +1,4 @@
+package com.lemos.controle_estoque.Repository;
+
+public class MovimentoEstoqueRepository {
+}
