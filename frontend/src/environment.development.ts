50aa7126d5460e15b3fd3aad7c7f68c3f571db3c
José Eduardo <jose.lemos.dev>
2026-07-17 10:07:32 -0300

    fix: removidos gets específicos e implementado get dinâmico

--- frontend/src/environment.development.ts
+++ frontend/src/environment.development.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://localhost:8080/',
+  produtoEndpoint : 'produto',
+  fornecedorEndpoint : 'fornecedor',
+  categoriaEndpoint : 'categoria',
+  movimentacaoEstoqueEndpoint : 'movimentacao-estoque',
 };
 
--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: true,
-  apiUrl: 'https://controle-estoque-s6yq.onrender.com/'
+  apiUrl: 'https://controle-estoque-s6yq.onrender.com/',
+  produtoEndpoint : 'produto',
+  fornecedorEndpoint : 'fornecedor',
+  categoriaEndpoint : 'categoria',
+  movimentacaoEstoqueEndpoint : 'movimentacao-estoque',
 };
