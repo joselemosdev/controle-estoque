16968577f9d066e40ead0f9a7b21ecbbb9246e1c
José Eduardo <jose.lemos.dev@gmail.com>
2026-07-22 20:19:15 -0300

    refact: implementado cards completos

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,46 +1,48 @@
 import { ChangeDetectorRef, Component, Inject, NgModule } from '@angular/core';
 import { DataComponentService } from '../dataComponentService';
 import { NgClass } from '@angular/common';
-import { MatIcon } from '@angular/material/icon'; // <-- Mude para MatIcon (sem Module)
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPlusCircle  } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgClass],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  
-  constructor(
-    private dataComponentService : DataComponentService,
-    
-  ){}
-    
-   get selectedOption(){
-    return this.dataComponentService.selectedOption();
-   }
 
-   public openLancamentos(){
+  constructor(
+    private dataComponentService: DataComponentService,
+
+  ) { }
+
+  get selectedOption() {
+    return this.dataComponentService.selectedOption();
+  }
+
+  public openLancamentos() {
     this.selectedOption != 'Lançamentos' ? this.dataComponentService.loadMovimentacaoEstoque() : null;
     this.dataComponentService.selectedOption.set('Lançamentos');
-    
-   }
-  
-   public openFornecedores(){
-     this.selectedOption != 'Fornecedores' ? this.dataComponentService.loadFornecedores() : null;
-     this.dataComponentService.selectedOption.set('Fornecedores');
 
-   }
-   public openCategorias(){
-     this.selectedOption != 'Categorias' ? this.dataComponentService.loadCategorias() : null;
-     this.dataComponentService.selectedOption.set('Categorias');
-   }
-   public openProdutos(){
-     this.selectedOption != 'Produtos' ? this.dataComponentService.loadProdutos() : null;
-     this.dataComponentService.selectedOption.set('Produtos');
-   }
+  }
+
+  public openFornecedores() {
+    this.selectedOption != 'Fornecedores' ? this.dataComponentService.loadFornecedores() : null;
+    this.dataComponentService.selectedOption.set('Fornecedores');
+
+  }
+  public openCategorias() {
+    this.selectedOption != 'Categorias' ? this.dataComponentService.loadCategorias() : null;
+    this.dataComponentService.selectedOption.set('Categorias');
+  }
+  public openProdutos() {
+    this.selectedOption != 'Produtos' ? this.dataComponentService.loadProdutos() : null;
+    this.dataComponentService.selectedOption.set('Produtos');
+  }
+
+  openUser() {
+
+  }
 
 }
