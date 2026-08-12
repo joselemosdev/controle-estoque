7268a20a04489f8669a4028bb8b46f43c44c99f3
José Eduardo <jose.lemos.dev@gmail.com>
2026-08-12 18:15:57 -0300

    implementando update

--- frontend/src/app/components/crud-modal/crud-modal.ts
+++ frontend/src/app/components/crud-modal/crud-modal.ts
@@ -1,5 +1,6 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Component, Inject, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -10,17 +11,23 @@ import { DataComponentService } from '../dataComponentService';
 @Component({
   selector: 'app-crud-modal',
   standalone: true,
-  imports: [FontAwesomeModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [FontAwesomeModule, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './crud-modal.html',
   styleUrl: './crud-modal.css',
 })
 export class CrudModal implements OnInit {
+
   constructor(
     @Inject(DIALOG_DATA) public data: any,
   ) {
     this.title = this.service.record!.titlePlural.toString();
   }
   isReadOnly = false;
+  crudForm = new FormGroup({});
+
+  get formFieldsAndValues(): Map<string, string> {
+    return this.itemFields;
+  }
 
   ngOnInit(): void {
     this.defineItemFields();
@@ -29,23 +36,25 @@ export class CrudModal implements OnInit {
   private defineItemFields() {
     //insere primeiro o id e depois os demais campos
     this.itemFields.set('ID', this.data.item.id);
+    const formControls: Record<string, FormControl> = {};
+
+
     Object.entries(this.data.item).forEach(([key, value]) => {
+
+      formControls[key] = new FormControl(value);
+
       if (key !== 'ID') {
         this.itemFields.set(key.toUpperCase(), String(value));
       }
+
     });
+
+    this.crudForm = new FormGroup(formControls);
 
     if (this.data.action == "view" || this.data.action == "delete") {
       this.isReadOnly = true;
     }
-
   }
-
-  private defineIdFirst() {
-    const id = this.itemFields.get('id');
-    this.itemFields;
-  }
-
 
 
   itemFields: Map<string, string> = new Map<string, string>();
@@ -62,9 +71,28 @@ export class CrudModal implements OnInit {
   protected cancel() {
     this.closeModal();
   }
-  protected confirmSave() {
-    this.closeModal();
 
+
+  protected confirmSave() {
+    if (this.data.action == 'edit') {
+      //update
+      console.log('values')
+      console.log(this.crudForm.value)
+      console.log(this.itemFields)
+      this.saveEdit(this.itemFields);
+      this.closeModal();
+      return;
+    }
+    this.saveNew(this.itemFields);
+    this.closeModal();
+    return;
+  }
+
+  private saveEdit(data: any) {
+    this.service.update(data);
+  }
+  private saveNew(data: any) {
+    this.service.saveNew(data);
   }
 
   protected confirmDelete() {
